7cc4267b51cb68cc71a14e80216fd9dcedc07fe1
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-02-27 15:15:46 -0300

    feat: authentication wiht JWT

---
+++ api-gateway/src/auth/service/auth.service.ts
@@ -0,0 +1,16 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly httpService: HttpService,
+  ) {}
+
+  validateJwtToken() {}
+  validateSessionToken() {}
+  login() {}
+  register() {}
+}
