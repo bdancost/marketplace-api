fe3c02778728c141af010494be50bc73b2d10a26
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-02-27 17:49:00 -0300

    feat: implemets service of authenticate

--- api-gateway/src/auth/service/auth.service.ts
+++ api-gateway/src/auth/service/auth.service.ts
@@ -1,6 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { firstValueFrom } from 'rxjs';
+import { serviceConfig } from 'src/config/gateway.config';
+
+export interface UserSession {
+  valid: boolean;
+  user: {
+    id: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    role: string;
+    status: string;
+  } | null;
+}
 
 @Injectable()
 export class AuthService {
@@ -9,8 +26,53 @@ export class AuthService {
     private readonly httpService: HttpService,
   ) {}
 
-  validateJwtToken() {}
-  validateSessionToken() {}
-  login() {}
-  register() {}
+  validateJwtToken(token: string): any {
+    try {
+      return this.jwtService.verify(token);
+    } catch (error) {
+      throw new UnauthorizedException('Invalid JWT token');
+    }
+  }
+  async validateSessionToken(sessionToken: string): Promise<UserSession> {
+    try {
+      const { data } = await firstValueFrom(
+        this.httpService.get<UserSession>(
+          `
+          ${serviceConfig.users.url}/session/validate/${sessionToken}`,
+          { timeout: serviceConfig.users.timeout },
+        ),
+      );
+      return data;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid session token');
+    }
+  }
+  async login(loginDto: { email: string; password: string }) {
+    try {
+      const { data } = await firstValueFrom(
+        this.httpService.post(`${serviceConfig.users.url}/login`, loginDto, {
+          timeout: serviceConfig.users.timeout,
+        }),
+      );
+      return data;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid login credentials');
+    }
+  }
+  async register(registerDto: any) {
+    try {
+      const { data } = await firstValueFrom(
+        this.httpService.post(
+          `${serviceConfig.users.url}/auth/register`,
+          registerDto,
+          {
+            timeout: serviceConfig.users.timeout,
+          },
+        ),
+      );
+      return data;
+    } catch (error) {
+      throw new UnauthorizedException('Registration failed');
+    }
+  }
 }
