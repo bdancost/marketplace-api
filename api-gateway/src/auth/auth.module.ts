238ad264e2c9af4c7839a2b7df4799611e14c806
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-02-28 17:35:11 -0300

    feat: implements auth controller

--- api-gateway/src/auth/auth.module.ts
+++ api-gateway/src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './service/auth.service';
+import { AuthController } from './controllers/auth.controller';
 
 @Module({
   imports: [
@@ -21,5 +22,6 @@ import { AuthService } from './service/auth.service';
   ],
   providers: [AuthService],
   exports: [],
+  controllers: [AuthController],
 })
 export class AuthModule {}
