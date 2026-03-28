550ff2029d94c8b5f84c020c59595e51f542d9d9
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-03-28 19:05:01 -0300

    feat: Implements Users Service and Api-gateway

---
+++ users-service/src/health/health.controller.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
+import { Public } from '../auth/decorators/public.decorator';
+
+@Controller('health')
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private db: TypeOrmHealthIndicator,
+  ) {}
+
+  @Public()
+  @Get()
+  @HealthCheck()
+  check() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    return this.health.check([() => this.db.pingCheck('database')]);
+  }
+}
