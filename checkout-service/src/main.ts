d8a9ab4a0858512c9927328b44288206e63bc253
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-04-02 14:23:04 -0300

    feat: spec and infrastructure checkout-service

--- checkout-service/src/main.ts
+++ checkout-service/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,9 +16,19 @@ async function bootstrap() {
     }),
   );
 
-  const port = process.env.PORT ?? 3003;
+  const config = new DocumentBuilder()
+    .setTitle('Checkout Service')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT || 3003;
   await app.listen(port);
 
-  console.log(`🛒 Checkout service running on port ${port}`);
+  console.log(`🛒 Checkout Service running on port ${port}`);
 }
+
 bootstrap();
